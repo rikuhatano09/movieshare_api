4413a69e9291a560d5e2da79ae0d4a518924bd9b
hatano-viviane <rikuhatano09@gmail.com>
2021-12-26 14:50:07 +0900

    feat: Update movie struct.

--- movieshare_api/pkg/domain/model/movie.go
+++ movieshare_api/pkg/domain/model/movie.go
@@ -2,10 +2,13 @@ package model
 
 type (
 	Movie struct {
-		ID             uint64 `json:"id" gorm:"column:id;type:bigserial;not null;primary_key"`
-		Title          string `json:"title" gorm:"column:title;type:text;not null"`
-		Overview       string `json:"overview" gorm:"column:overview;type:text;not null"`
-		Genre          string `json:"genre" gorm:"column:genre;type:text;not null"`
-		YouTubeLinkURL string `json:"youtubeLinkUrl" gorm:"column:youtube_link_url;type:text;not null"`
+		ID                  uint64  `json:"id" gorm:"column:id;type:bigserial;not null;primary_key"`
+		UserID              uint64  `json:"userId" gorm:"column:user_id;type:bigint;not null"`
+		Title               string  `json:"title" gorm:"column:title;type:text;not null"`
+		Overview            string  `json:"overview" gorm:"column:overview;type:text;not null"`
+		Genre               string  `json:"genre" gorm:"column:genre;type:text;not null"`
+		YouTubeLinkURL      string  `json:"youtubeLinkUrl" gorm:"column:youtube_link_url;type:text;not null"`
+		YouTubeThumbnailURL string  `json:"youtubeThumbnailUrl" gorm:"column:youtube_thumbnail_url;type:text;not null"`
+		GrinningScore       *uint32 `json:"grinningScore" gorm:"column:grinning_score;type:integer"`
 	}
 )
