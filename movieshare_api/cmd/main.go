e7ce3a6d808b611121e83d00c86f01189046aa0c
hatano-viviane <rikuhatano09@gmail.com>
2021-12-26 15:35:14 +0900

    feat: Set cros config.

--- movieshare_api/cmd/main.go
+++ movieshare_api/cmd/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
 	"github.com/rikuhatano09/movieshare_api/internal/interfaces/handler"
 )
 
 func main() {
 	engine := gin.Default()
+
+	// Set CORS config.
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+	engine.Use(cors.New(corsConfig))
 
 	engine.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
