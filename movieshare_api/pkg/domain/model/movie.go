166acd878d1e2b961d76aad13c432a27c2c61cc6
yukiimaii <yuki03262000@gmail.com>
2021-12-26 17:57:22 +0900

    update: add logic for getting by id

--- movieshare_api/pkg/domain/model/movie.go
+++ movieshare_api/pkg/domain/model/movie.go
@@ -2,7 +2,7 @@ package model
 
 type (
 	Movie struct {
-		Id             uint64 `json:"id" gorm:"column:id;type:bigserial;not null;primary_key"`
+		ID             uint64 `json:"id" gorm:"column:id;type:bigserial;not null;primary_key"`
 		Title          string `json:"title" gorm:"column:title;type:text;not null"`
 		Overview       string `json:"overview" gorm:"column:overview;type:text;not null"`
 		Genre          string `json:"genre" gorm:"column:genre;type:text;not null"`
