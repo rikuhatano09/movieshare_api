eeae9fc573704c40bba5c6796467d8ec9397d8e1
hatano-viviane <rikuhatano09@gmail.com>
2021-12-26 14:12:57 +0900

    feat: Update database definition.

--- movieshare_database/definition/1_create_tables.sql
+++ movieshare_database/definition/1_create_tables.sql
@@ -1,12 +1,15 @@
 -- Movie
 DROP   TABLE    IF     EXISTS "public"."movie" CASCADE;
 CREATE TABLE    IF NOT EXISTS "public"."movie" (
-    "id"               BIGSERIAL                NOT NULL,
-    "created_at"       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "updated_at"       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "title"            TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("title") > 0),
-    "overview"         TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("overview") > 0),
-    "genre"            TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("genre") > 0),
-    "youtube_link_url" TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("youtube_link_url") > 0),
+    "id"                    BIGSERIAL                NOT NULL,
+    "created_at"            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "updated_at"            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "user_id"               BIGINT                   NOT NULL,
+    "title"                 TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("title") > 0),
+    "overview"              TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("overview") > 0),
+    "genre"                 TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("genre") > 0),
+    "youtube_link_url"      TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("youtube_link_url") > 0),
+    "youtube_thumbnail_url" TEXT                     NOT NULL CHECK (CHARACTER_LENGTH("youtube_thumbnail_url") > 0),
+    "grinning_score"        INTEGER                           CHECK ("grinning_score" > 0),
     PRIMARY KEY ("id")
 );
--- movieshare_database/definition/3_insert_master_data.sql
+++ movieshare_database/definition/3_insert_master_data.sql
@@ -1,9 +1,9 @@
 -- Movie
 INSERT INTO "public"."movie"
-    ("title", "overview", "genre", "youtube_link_url")
+    ("user_id", "title", "overview", "genre", "youtube_link_url", "youtube_thumbnail_url", "grinning_score")
 VALUES
-    ('マトリックス レザレクションズ', 'この冬、マトリックスが世界を変える！主演キアヌ・リーブスが人生をかけて挑む、全世界で空前の社会現象を巻き起こした今世紀最大のアクション超大作新章！', 'SF', 'https://www.youtube.com/watch?v=8PwqzRR3Oo0'),
-    ('ジョーカー', '「どんな時も笑顔で人々を楽しませなさい」という母の言葉を胸にコメディアンを夢見る、孤独だが心優しいアーサー。', 'サスペンス', 'https://www.youtube.com/watch?v=C3nQcMM5fS4'),
-    ('ダークナイト', 'アメコミ映画を変えた、あの伝説を劇場で体感せよ！', 'アクション', 'https://www.youtube.com/watch?v=2gnl7JxTC0g'),
-    ('インターステラー', 'クリストファー・ノーラン監督待望の最新作『インターステラー』は、新たに発見された宇宙のワームホールを利用し、惑星間を旅する人類の限界を超越した冒険を追っていくストーリーが描かれる。ノーランの原案と、ジョナサン・ノーランの既存の脚本を融合させたプロットを基に、最終的な脚本が作られた。', 'SF', 'https://www.youtube.com/watch?v=qZZ9jRan9eo')
+    (1, 'マトリックス レザレクションズ', 'この冬、マトリックスが世界を変える！主演キアヌ・リーブスが人生をかけて挑む、全世界で空前の社会現象を巻き起こした今世紀最大のアクション超大作新章！', 'SF', 'https://www.youtube.com/watch?v=8PwqzRR3Oo0', 'https://i.ytimg.com/vi/8PwqzRR3Oo0/mqdefault.jpg', NULL),
+    (1, 'ジョーカー', '「どんな時も笑顔で人々を楽しませなさい」という母の言葉を胸にコメディアンを夢見る、孤独だが心優しいアーサー。', 'サスペンス', 'https://www.youtube.com/watch?v=C3nQcMM5fS4', 'https://i.ytimg.com/vi/C3nQcMM5fS4/mqdefault.jpg', NULL),
+    (1, 'ダークナイト', 'アメコミ映画を変えた、あの伝説を劇場で体感せよ！', 'アクション', 'https://www.youtube.com/watch?v=2gnl7JxTC0g', 'https://i.ytimg.com/vi/2gnl7JxTC0g/mqdefault.jpg', NULL),
+    (1, 'インターステラー', 'クリストファー・ノーラン監督待望の最新作『インターステラー』は、新たに発見された宇宙のワームホールを利用し、惑星間を旅する人類の限界を超越した冒険を追っていくストーリーが描かれる。ノーランの原案と、ジョナサン・ノーランの既存の脚本を融合させたプロットを基に、最終的な脚本が作られた。', 'SF', 'https://www.youtube.com/watch?v=qZZ9jRan9eo', 'https://i.ytimg.com/vi/qZZ9jRan9eo/mqdefault.jpg', NULL)
 ;
